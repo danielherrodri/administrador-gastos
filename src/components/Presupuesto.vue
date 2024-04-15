e2bce9da7b311325ade4f77ddbcd52879fb374da
danielherrodri <84465666+danielherrodri@users.noreply.github.com>
2024-04-14 21:33:26 -0600

    Vídeo 106 - Finalizando el formulario del presupuesto

--- src/components/Presupuesto.vue
+++ src/components/Presupuesto.vue
@@ -1,11 +1,11 @@
 <script setup></script>
 
 <template>
-  <form action="">
+  <form class="presupuesto" action="">
     <div class="campo">
       <label for="">Definir Presupuesto</label>
       <input
-        type="text"
+        type="number"
         id="nuevo-presupuesto"
         name="nuevo-presupuesto"
         placeholder="Añade tu presupuesto"
@@ -15,4 +15,47 @@
   </form>
 </template>
 
-<style scoped></style>
+<style scoped>
+.presupuesto {
+  width: 100%;
+}
+.alerta {
+  background-color: red;
+}
+.campo {
+  display: grid;
+  gap: 2rem;
+}
+.presupuesto input[type="number"] {
+  background-color: var(--gris-claro);
+  border-radius: 1rem;
+  padding: 1rem;
+  border: none;
+  font-size: 2.2rem;
+  text-align: center;
+}
+
+.presupuesto input[type="submit"] {
+  background-color: var(--azul);
+  border: none;
+  padding: 1rem;
+  text-align: center;
+  font-size: 2rem;
+  margin-top: 2rem;
+  color: var(--blanco);
+  font-weight: 900;
+  width: 100%;
+  transition: background-color 300ms ease;
+}
+
+.presupuesto input[type="submit"]:hover {
+  background-color: #1048a4;
+  cursor: pointer;
+}
+
+.presupuesto label {
+  font-size: 2.8rem;
+  text-align: center;
+  color: var(--azul);
+}
+</style>
