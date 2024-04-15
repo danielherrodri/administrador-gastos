6948223ab5663a8f6a7d5e75cbe1e62941948ce0
danielherrodri <84465666+danielherrodri@users.noreply.github.com>
2024-04-14 20:54:02 -0600

    Vídeo 103 - Como funciona style en los single file components

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,11 @@
 </script>
 
 <template>
-
+  <div>
+    <Header>
+      <h1>Planificador de gastos</h1>
+    </Header>
+  </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style></style>
