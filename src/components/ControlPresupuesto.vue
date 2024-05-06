c175b714bff91bb388cc60f7c171bc7b659a5a88
danielherrodri <84465666+danielherrodri@users.noreply.github.com>
2024-05-05 22:23:34 -0600

    Vídeo 111 - CSS al panel de presupuesto.

--- src/components/ControlPresupuesto.vue
+++ src/components/ControlPresupuesto.vue
@@ -10,17 +10,67 @@ import imagen from "../assets/img/grafico.jpg";
 
     <div class="contenedor-presupuesto">
       <button class="reset-app">Resetear App</button>
-      <p>
-        <span>Presupuesto: $0</span>
-      </p>
-      <p>
-        <span>Disponible: $0</span>
-      </p>
-      <p>
-        <span>Gastado: $0</span>
-      </p>
+      <p><span>Presupuesto:</span>$0</p>
+      <p><span>Disponible:</span>$0</p>
+      <p><span>Gastado:</span>$0</p>
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.dos-columnas {
+  display: flex;
+  flex-direction: column;
+}
+
+.dos-columnas > :first-child {
+  margin-bottom: 3rem;
+}
+
+.contenedor-presupuesto {
+  width: 100%;
+}
+
+.contenedor-presupuesto p {
+  font-size: 2.4rem;
+  text-align: center;
+  color: var(--gris-oscuro);
+}
+
+.contenedor-presupuesto span {
+  font-weight: 900;
+  color: var(--azul);
+}
+
+.reset-app {
+  background-color: #db2777;
+  border: none;
+  padding: 1rem;
+  width: 100%;
+  color: var(--blanco);
+  font-weight: 900;
+  text-transform: uppercase;
+  border-radius: 1rem;
+  transition-property: background-color;
+  transition-duration: 300ms;
+}
+
+.reset-app:hover {
+  cursor: pointer;
+  background-color: hsl(333, 78%, 38%);
+}
+
+@media (min-width: 768px) {
+  .dos-columnas {
+    flex-direction: row;
+    gap: 4rem;
+    align-items: center;
+  }
+  .dos-columnas > :first-child {
+    margin-bottom: 0;
+  }
+  .contenedor-presupuesto p {
+    text-align: left;
+  }
+}
+</style>
