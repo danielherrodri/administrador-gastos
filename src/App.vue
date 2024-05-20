bae5f0787e4d2012dffbc017006018d619622b7c
drodriguez <drodriguez@izzi.mx>
2024-05-19 22:50:07 -0600

    Vídeo 124 - Almacenando gastos en el state

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,8 @@ const gasto = reactive({
   fecha: Date.now()
 })
 
+const gastos = ref([])
+
 const definirPresupuesto = (cantidad) => {
   presupuesto.value = cantidad;
   disponible.value = cantidad;
@@ -41,7 +43,10 @@ const ocultarModal = () => {
 }
 
 const guardarGasto = () => {
-  console.log('Desde app.vue')
+  gastos.value.push({
+    id: 123,
+    ...gasto
+  })
 }
 </script>
 
